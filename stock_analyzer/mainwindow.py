a5d786547662f7421099ad1e0de79d2b090a3df1
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-11-28 18:37:04 -0500

    Read data from file

--- stock_analyzer/mainwindow.py
+++ stock_analyzer/mainwindow.py
@@ -1,10 +1,16 @@
-from PySide6 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
+import csv
+
+from PySide6 import QtCore, QtWidgets, QtWebEngineWidgets
+
+from stock_analyzer.btree import BTree
+from stock_analyzer.marketday import MarketDay
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.tree = None
         self.setWindowTitle("Stock Analyzer")
 
         self.countryFilter = QtWidgets.QComboBox()
@@ -13,8 +19,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.highlightedFeature = QtWidgets.QComboBox()
         self.highlightedFeature.setPlaceholderText("Highlighted Feature")
 
-        self.dateFilter = QtWidgets.QComboBox()
-        self.dateFilter.setPlaceholderText("Date Filter")
+        self.dateFilterStart = QtWidgets.QDateEdit(QtCore.QDate(2022, 1, 1))
+
+        self.dateFilterLabel = QtWidgets.QLabel()
+        self.dateFilterLabel.setText("to")
+        shrink = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
+        self.dateFilterLabel.setSizePolicy(shrink)
+
+
+        self.dateFilterEnd = QtWidgets.QDateEdit(QtCore.QDate(2023, 1, 1))
 
         self.dataStructure = QtWidgets.QComboBox()
         self.dataStructure.addItems(["B Tree", "B+ Tree", "Red-Black Tree"])
@@ -28,7 +41,9 @@ class MainWindow(QtWidgets.QMainWindow):
         comboBoxLayout = QtWidgets.QHBoxLayout()
         comboBoxLayout.addWidget(self.countryFilter)
         comboBoxLayout.addWidget(self.highlightedFeature)
-        comboBoxLayout.addWidget(self.dateFilter)
+        comboBoxLayout.addWidget(self.dateFilterStart)
+        comboBoxLayout.addWidget(self.dateFilterLabel)
+        comboBoxLayout.addWidget(self.dateFilterEnd)
         comboBoxLayout.addWidget(self.dataStructure)
 
         bottomLayout = QtWidgets.QHBoxLayout()
@@ -47,3 +62,21 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def loadMap(self, url):
         self.webMapView.load(url)
+
+    def createMap(self):
+        match self.dataStructure.currentIndex():
+            case 0:
+                self.tree = BTree(5, 4)
+            # case 1:
+            #     self.tree = BPlusTree(5, 4)
+            # case 2:
+            #     self.tree = RBTree()
+            case _:
+                return
+
+        with open("data.csv") as file:
+            reader = csv.reader(file)
+            for r in reader:
+                # some columns (stock splits, brand name) are skipped
+                dataPoint = MarketDay(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[9], r[10], r[11])
+                self.tree.insert(dataPoint)
--- stock_analyzer/marketday.py
+++ stock_analyzer/marketday.py
@@ -3,8 +3,9 @@ import time
 
 class MarketDay:
     """A class that represents a single data point."""
-    def __init__(self, date_string, high, low, close, vol, dividends, ticker, industry, country):
+    def __init__(self, date_string, open_price, high, low, close, vol, dividends, ticker, industry, country):
         self.date = time.strptime(date_string, "%Y-%m-%d %H:%M:%S%z")
+        self.open = open_price
         self.high = high
         self.low = low
         self.close = close
