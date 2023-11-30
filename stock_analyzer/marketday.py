28b340fbb6e8da2a38c38391f3f1fa221ca123ff
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-11-30 17:58:21 -0500

    MarketDay: equality by value, convert numbers from str to float

--- stock_analyzer/marketday.py
+++ stock_analyzer/marketday.py
@@ -5,13 +5,13 @@ class MarketDay:
     """A class that represents a single data point."""
     def __init__(self, date_string, open_price, high, low, close, vol, dividends, splits, ticker, industry, country):
         self.date = time.strptime(date_string, "%Y-%m-%d %H:%M:%S%z")
-        self.open = open_price
-        self.high = high
-        self.low = low
-        self.close = close
-        self.vol = vol
-        self.dividends = dividends
-        self.splits = splits
+        self.open = float(open_price)
+        self.high = float(high)
+        self.low = float(low)
+        self.close = float(close)
+        self.vol = float(vol)
+        self.dividends = float(dividends)
+        self.splits = float(splits)
         self.ticker = ticker
         self.industry = industry
         self.country = country
@@ -21,3 +21,18 @@ class MarketDay:
 
     def __le__(self, other):
         return self.date <= other.date
+
+    def __eq__(self, other):
+        if isinstance(other, MarketDay):
+            return (self.date == other.date and
+                    self.open == other.open and
+                    self.high == other.high and
+                    self.low == other.low and
+                    self.close == other.close and
+                    self.vol == other.vol and
+                    self.dividends == other.dividends and
+                    self.splits == other.splits and
+                    self.ticker == other.ticker and
+                    self.industry == other.industry and
+                    self.country == other.country)
+        return False
