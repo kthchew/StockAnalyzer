dcbcaecd0ed5cef8c477b89b75149c8d2b84f30f
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-11-29 23:22:11 -0500

    Create tree when data structure chosen in UI

--- stock_analyzer/mainwindow.py
+++ stock_analyzer/mainwindow.py
@@ -1,4 +1,5 @@
 import csv
+import time
 
 from PySide6 import QtCore, QtWidgets, QtWebEngineWidgets
 
@@ -26,17 +27,22 @@ class MainWindow(QtWidgets.QMainWindow):
         shrink = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
         self.dateFilterLabel.setSizePolicy(shrink)
 
-
         self.dateFilterEnd = QtWidgets.QDateEdit(QtCore.QDate(2023, 1, 1))
 
         self.dataStructure = QtWidgets.QComboBox()
         self.dataStructure.addItems(["B Tree", "B+ Tree", "Red-Black Tree"])
+        self.dataStructure.setCurrentIndex(-1)
+        self.dataStructure.setPlaceholderText("Data Structure")
+        self.dataStructure.currentIndexChanged.connect(self.timeCreateTree)
 
         self.webMapView = QtWebEngineWidgets.QWebEngineView()
         self.webMapView.load(QtCore.QUrl("https://example.com"))
 
-        self.timerLabel = QtWidgets.QLabel()
-        self.timerLabel.setText("Time to create map: ?")
+        self.mapTimerLabel = QtWidgets.QLabel()
+        self.mapTimerLabel.setText("Time to create map: ?")
+
+        self.treeTimerLabel = QtWidgets.QLabel()
+        self.treeTimerLabel.setText("Time to create tree: ?")
 
         comboBoxLayout = QtWidgets.QHBoxLayout()
         comboBoxLayout.addWidget(self.countryFilter)
@@ -49,7 +55,8 @@ class MainWindow(QtWidgets.QMainWindow):
         bottomLayout = QtWidgets.QHBoxLayout()
         spacer = QtWidgets.QSpacerItem(0, 0, QtWidgets.QSizePolicy.Policy.MinimumExpanding)
         bottomLayout.addSpacerItem(spacer)
-        bottomLayout.addWidget(self.timerLabel)
+        bottomLayout.addWidget(self.mapTimerLabel)
+        bottomLayout.addWidget(self.treeTimerLabel)
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addLayout(comboBoxLayout)
@@ -63,7 +70,14 @@ class MainWindow(QtWidgets.QMainWindow):
     def loadMap(self, url):
         self.webMapView.load(url)
 
-    def createMap(self):
+    def timeCreateTree(self, func):
+        """Times the tree creation and adds the time in seconds to the UI."""
+        start = time.time()
+        self.createTree()
+        end = time.time()
+        self.treeTimerLabel.setText("Time to create tree: " + str(round(end - start, 2)) + " sec")
+
+    def createTree(self):
         match self.dataStructure.currentIndex():
             case 0:
                 self.tree = BTree(5, 4)
@@ -77,6 +91,9 @@ class MainWindow(QtWidgets.QMainWindow):
         with open("data.csv") as file:
             reader = csv.reader(file)
             for r in reader:
-                # some columns (stock splits, brand name) are skipped
-                dataPoint = MarketDay(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[9], r[10], r[11])
+                # skip header row
+                if r[0] == "Date":
+                    continue
+                # column brand name is skipped
+                dataPoint = MarketDay(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[9], r[10], r[11])
                 self.tree.insert(dataPoint)
