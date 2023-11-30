de22fb3ef9dc51085cf0c503c8687f497874f6c2
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-11-29 23:36:09 -0500

    Add buttons to create tree and map view

--- stock_analyzer/mainwindow.py
+++ stock_analyzer/mainwindow.py
@@ -19,6 +19,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.highlightedFeature = QtWidgets.QComboBox()
         self.highlightedFeature.setPlaceholderText("Highlighted Feature")
+        self.highlightedFeature.addItems(["Trading Volume", "Gains/Losses"])
 
         self.dateFilterStart = QtWidgets.QDateEdit(QtCore.QDate(2022, 1, 1))
 
@@ -31,9 +32,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.dataStructure = QtWidgets.QComboBox()
         self.dataStructure.addItems(["B Tree", "B+ Tree", "Red-Black Tree"])
-        self.dataStructure.setCurrentIndex(-1)
         self.dataStructure.setPlaceholderText("Data Structure")
-        self.dataStructure.currentIndexChanged.connect(self.timeCreateTree)
+
+        self.makeTreeButton = QtWidgets.QPushButton("Build Tree")
+        self.makeTreeButton.clicked.connect(self.timeCreateTree)
+
+        self.makeMapButton = QtWidgets.QPushButton("Build Map")
+        self.makeMapButton.clicked.connect(self.timeCreateMap)
+        self.makeMapButton.setEnabled(False)
 
         self.webMapView = QtWebEngineWidgets.QWebEngineView()
         self.webMapView.load(QtCore.QUrl("https://example.com"))
@@ -52,6 +58,10 @@ class MainWindow(QtWidgets.QMainWindow):
         comboBoxLayout.addWidget(self.dateFilterEnd)
         comboBoxLayout.addWidget(self.dataStructure)
 
+        buttonLayout = QtWidgets.QHBoxLayout()
+        buttonLayout.addWidget(self.makeTreeButton)
+        buttonLayout.addWidget(self.makeMapButton)
+
         bottomLayout = QtWidgets.QHBoxLayout()
         spacer = QtWidgets.QSpacerItem(0, 0, QtWidgets.QSizePolicy.Policy.MinimumExpanding)
         bottomLayout.addSpacerItem(spacer)
@@ -60,6 +70,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addLayout(comboBoxLayout)
+        self.layout.addLayout(buttonLayout)
         self.layout.addWidget(self.webMapView)
         self.layout.addLayout(bottomLayout)
 
@@ -70,7 +81,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def loadMap(self, url):
         self.webMapView.load(url)
 
-    def timeCreateTree(self, func):
+    def timeCreateTree(self):
         """Times the tree creation and adds the time in seconds to the UI."""
         start = time.time()
         self.createTree()
@@ -97,3 +108,16 @@ class MainWindow(QtWidgets.QMainWindow):
                 # column brand name is skipped
                 dataPoint = MarketDay(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[9], r[10], r[11])
                 self.tree.insert(dataPoint)
+
+        self.makeMapButton.setEnabled(True)
+
+    def timeCreateMap(self):
+        start = time.time()
+        url = self.createMap()
+        self.loadMap(url)
+        end = time.time()
+        self.mapTimerLabel.setText("Time to create map: " + str(round(end - start, 2)) + " sec")
+
+    def createMap(self):
+        # TODO: actually create map here
+        return "https://www.example.com"
