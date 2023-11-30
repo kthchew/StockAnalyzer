42237097cde81f6c64c091dc8f4ba7b357210092
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-11-30 18:01:37 -0500

    Test B Tree find

--- stock_analyzer/tests/btreetests.py
+++ stock_analyzer/tests/btreetests.py
@@ -1,6 +1,8 @@
+import csv
 import unittest
 
 from stock_analyzer.btree import BTree
+from stock_analyzer.marketday import MarketDay
 
 
 class BTreeTests(unittest.TestCase):
@@ -30,6 +32,29 @@ class BTreeTests(unittest.TestCase):
         self.assertEqual(tree.root.children[0].children[1].items, [4])
         self.assertEqual(tree.root.children[1].children[0].items, [6])
         self.assertEqual(tree.root.children[1].children[1].items, [11, 17])
+
+    def test_find(self):
+        tree = BTree(3, 2)
+        with open("test.csv") as file:
+            reader = csv.reader(file)
+            for r in reader:
+                # skip header row
+                if r[0] == "Date":
+                    continue
+                # column brand name is skipped
+                data_point = MarketDay(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[9], r[10], r[11])
+                tree.insert(data_point)
+
+        data_point = MarketDay("2023-09-20 00:00:00-04:00", "564.3499755859375",
+                              "569.219970703125", "562.6599731445312", "563.8300170898438",
+                              "1311500.0", "0.0", "0.0", "COST", "retail",
+                              "usa")
+        data_point2 = MarketDay("2023-06-23 00:00:00-04:00", "21.71629018950936",
+                               "26.01165263931803","25.302781633056032", "25.529226303100582",
+                               "3230400.0", "0.0", "0.0", "FL", "footwear", "usa")
+
+        self.assertTrue(tree.find(data_point))
+        self.assertFalse(tree.find(data_point2))
 
 
 if __name__ == '__main__':
