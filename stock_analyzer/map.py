21f9338586f11289b37f673ecd826e2e768a311b
MonsieurMantis <116741888+MonsieurMantis@users.noreply.github.com>
2023-12-03 16:29:59 -0500

    added a working nan_color function
    temporary dict is in there to see if it works. The kinks haven't been worked out as seen with the US

--- stock_analyzer/map.py
+++ stock_analyzer/map.py
@@ -1,43 +1,111 @@
 import folium
 import requests
-import random
+import folium.plugins
+import branca
+import branca.colormap as cm
 
 
-# Maps low values to green and high values to red.
-# We can change this to varying shades depending on how successful stocks are doing later
-def my_color_function():
-    if random.random() > .5:
-        return "#ff0000"
+def nanFunc(feat, Dict):
+
+    if Dict.get(feat["properties"]["name"].lower(), -1) != -1:
+        return linear(Dict[feat["properties"]["name"].lower()])
     else:
-        return "#008000"
+        return "white"
 
-def testMap():
 
-    m = folium.Map(tiles=folium.TileLayer(no_wrap=True),
-                   zoom_control=True,
-                   control_scale=True,
-                   zoom_start=20,
-                   max_zoom=20,
-                   )
+linear = cm.linear.Spectral_07
+step = cm.linear.Spectral_07.to_step(12)
 
-    geojson_data = requests.get(
-        "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/world_countries.json"
-    ).json()
+m = folium.Map(tiles=folium.TileLayer(name="World Map", no_wrap=True),
+               zoom_control=True,
+               control_scale=True,
+               zoom_start=20,
+               max_zoom=20,
+               )
 
-    geoJson = folium.GeoJson(geojson_data,
-                             style_function=lambda feature: {
-                                 "fillColor": my_color_function(),
-                                 "color": "black",
-                                 "weight": 2,
-                                 "dashArray": "5, 5",
-                             },
-                             name="Stock Analysis",
-                             legend_name="Stonks",
-                             highlight=True,
-                             show=True,
-                             zoom_on_click=True,
-                             ).add_to(m)
+geojson_data = requests.get(
+    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/world_countries.json"
+).json()
 
-    folium.LayerControl().add_to(m)
+Dict = {"france": .05, "usa": .2, "japan": .9}
 
-    m.save("output_map.html")
+geoJson1 = folium.GeoJson(geojson_data,
+                          style_function=lambda feature: {
+                              "fillColor": nanFunc(feature, Dict),
+                              "color": "black",
+                              "weight": 2,
+                              "dashArray": "5, 5",
+                              "nan_fill_color": "white",
+                              "nan_fill_opacity": 0.4,
+                          },
+                          highlight_function=lambda feature: {
+                              "color": "black",
+                              "weight": 3,
+                              "dashArray": "0, 0",
+                          },
+                          name="High",
+                          highlight=True,
+                          show=True,
+                          zoom_on_click=True,
+                          ).add_to(m)
+
+geoJson2 = folium.GeoJson(geojson_data,
+                          style_function=lambda feature: {
+                              "fillColor": nanFunc(feature, Dict),
+                              "color": "black",
+                              "weight": 2,
+                              "dashArray": "5, 5",
+                              "nan_fill_color": "white",
+                              "nan_fill_opacity": 0.4,
+                          },
+                          highlight_function=lambda feature: {
+                              "color": "black",
+                              "weight": 4,
+                          },
+                          name="Low",
+                          highlight=True,
+                          show=False,
+                          zoom_on_click=True,
+                          ).add_to(m)
+
+geoJson3 = folium.GeoJson(geojson_data,
+                          style_function=lambda feature: {
+                              "fillColor": nanFunc(feature, Dict),
+                              "color": "black",
+                              "weight": 2,
+                              "dashArray": "5, 5",
+                              "nan_fill_color": "white",
+                              "nan_fill_opacity": 0.4,
+                          },
+                          highlight_function=lambda feature: {
+                              "color": "black",
+                              "weight": 4,
+                          },
+                          name="Volatility",
+                          highlight=True,
+                          show=False,
+                          zoom_on_click=True,
+                          ).add_to(m)
+
+geoJson4 = folium.GeoJson(geojson_data,
+                          style_function=lambda feature: {
+                              "fillColor": nanFunc(feature, Dict),
+                              "color": "black",
+                              "weight": 2,
+                              "dashArray": "5, 5",
+                              "nan_fill_color": "white",
+                              "nan_fill_opacity": 0.4,
+                          },
+                          highlight_function=lambda feature: {
+                              "color": "black",
+                              "weight": 4,
+                          },
+                          name="Volume",
+                          highlight=True,
+                          show=False,
+                          zoom_on_click=True,
+                          ).add_to(m)
+
+folium.LayerControl().add_to(m)
+
+m.save("index.html")
