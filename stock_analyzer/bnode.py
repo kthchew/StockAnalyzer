8eb678932cae44e9aa8470da1cba2e2dabc648d5
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-12-03 21:51:14 -0500

    Change B tree to use camelCase

--- stock_analyzer/bnode.py
+++ stock_analyzer/bnode.py
@@ -3,53 +3,53 @@ class Node:
         self.items = items
         self.children = children
 
-    def child_count(self):
+    def childCount(self):
         """Return the number of child nodes."""
         return len(self.children)
 
-    def item_count(self):
+    def itemCount(self):
         """Return the number of items in the node."""
         return len(self.items)
 
-    def is_leaf(self):
+    def isLeaf(self):
         """Return whether this node is a leaf node."""
         return len(self.children) == 0
 
-    def add_item(self, item):
+    def addItem(self, item):
         """Add the given item to this node in the proper order."""
-        for i in range(self.item_count()):
+        for i in range(self.itemCount()):
             if item <= self.items[i]:
                 self.items.insert(i, item)
                 return
         self.items.append(item)
 
-    def split_node(self):
+    def splitNode(self):
         """Return a node containing the left half of this node's elements, the middle element of
         this node, and a node containing the right half. Neither node will contain the middle
         element."""
-        middle_index = self.item_count() // 2
+        middle_index = self.itemCount() // 2
         middle = self.items[middle_index]
         left = []
         right = []
         for i in range(middle_index):
             left.append(self.items[i])
-        for i in range(middle_index + 1, self.item_count()):
+        for i in range(middle_index + 1, self.itemCount()):
             right.append(self.items[i])
 
         left_children = []
         right_children = []
-        if not self.is_leaf():
+        if not self.isLeaf():
             for i in range(len(left) + 1):
                 left_children.append(self.children[i])
-            for i in range(len(left) + 1, self.child_count()):
+            for i in range(len(left) + 1, self.childCount()):
                 right_children.append(self.children[i])
 
         return Node(left, left_children), middle, Node(right, right_children)
 
-    def split_child(self, index):
+    def splitChild(self, index):
         """Split the indexth child (starting at 0) into two children, and moves the middle
         element in that child to the current node."""
-        left, middle, right = self.children[index].split_node()
+        left, middle, right = self.children[index].splitNode()
         self.children[index] = left
         self.children.insert(index + 1, right)
-        self.add_item(middle)
+        self.addItem(middle)
--- stock_analyzer/btree.py
+++ stock_analyzer/btree.py
@@ -17,23 +17,23 @@ class BTree:
         """Insert the provided item in the tree. Nodes are automatically split if necessary."""
         path = []
         current: Node = self.root
-        while not current.is_leaf():
-            next_index = 0
-            for i in range(current.item_count()):
+        while not current.isLeaf():
+            nextIndex = 0
+            for i in range(current.itemCount()):
                 if item > current.items[i]:
-                    next_index += 1
+                    nextIndex += 1
                 else:
                     break
-            path.append((current, next_index))
-            current = current.children[next_index]
+            path.append((current, nextIndex))
+            current = current.children[nextIndex]
 
-        current.add_item(item)
+        current.addItem(item)
         for node, child in reversed(path):
-            child_node = node.children[child]
-            if child_node.item_count() > self.l or child_node.child_count() > self.n:
-                node.split_child(child)
-        if self.root.item_count() > self.l:
-            left, mid, right = self.root.split_node()
+            childNode = node.children[child]
+            if childNode.itemCount() > self.l or childNode.childCount() > self.n:
+                node.splitChild(child)
+        if self.root.itemCount() > self.l:
+            left, mid, right = self.root.splitNode()
             mid = Node([mid], [left, right])
             self.root = mid
 
@@ -43,19 +43,31 @@ class BTree:
         queue.put(self.root)
         while not queue.empty():
             current = queue.get()
-            for i in range(current.item_count()):
+            for i in range(current.itemCount()):
                 if item == current.items[i]:
                     return True
 
-                if i == 0 and item <= current.items[i] and not current.is_leaf():
+                if i == 0 and item <= current.items[i] and not current.isLeaf():
                     queue.put(current.children[i])
-                if item >= current.items[i] and not current.is_leaf():
+                if item >= current.items[i] and not current.isLeaf():
                     queue.put(current.children[i + 1])
                 else:
                     break
         return False
 
-    def runDateFilter(self, start_date: time.struct_time, end_date: time.struct_time, function, *args):
+    def printTree(self):
+        """Return True if the item is found in the tree, False otherwise."""
+        queue = Queue()
+        queue.put(self.root)
+        total = 0
+        while not queue.empty():
+            current = queue.get()
+            total += current.itemCount()
+            for child in current.children:
+                queue.put(child)
+        print(total)
+
+    def runDateFilter(self, startDate: time.struct_time, endDate: time.struct_time, function, *args):
         """Runs the provided function on all items between the start and end date.
         The function's first argument is the item in the tree to process. Other arguments provided to this method
         are passed into the provided function."""
@@ -63,16 +75,16 @@ class BTree:
         queue.put(self.root)
         while not queue.empty():
             current: Node = queue.get()
-            for i in range(current.item_count()):
+            for i in range(current.itemCount()):
                 item = current.items[i]
 
-                if start_date <= item.date <= end_date:
+                if startDate <= item.date <= endDate:
                     function(item, *args)
 
                 # things to the left have an older date, so check this is at least the start date (if not, then useful
                 # items can't possibly be to the left)
                 # similar logic with searching to the right
-                if i == 0 and item.date >= start_date and not current.is_leaf():
+                if i == 0 and item.date >= startDate and not current.isLeaf():
                     queue.put(current.children[i])
-                if item.date <= end_date and not current.is_leaf():
+                if item.date <= endDate and not current.isLeaf():
                     queue.put(current.children[i + 1])
